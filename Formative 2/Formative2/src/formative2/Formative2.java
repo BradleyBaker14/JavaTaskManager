21af6bac7cbfb957b613a3fbb32c6c6bfc81a1ae
Bradley Baker <bradsgbaker14@gmail.com>
2023-11-23 16:21:23 +0200

    Intitial Commit

---
+++ Formative 2/Formative2/src/formative2/Formative2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package formative2;
+
+import formative2.GUIApplication;
+
+/**
+ *
+ * @author brads
+ */
+public class Formative2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GUIApplication guiApp = new GUIApplication();
+        guiApp.setVisible(true);
+    }
+    
+}
